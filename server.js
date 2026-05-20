a01f2133c01793052cae2de1d3a932fa8d2b8518
bulbox699 <cartegrisefrance6@gmail.com>
2026-05-20 19:04:04 +0200

    go

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 /* =========================
    DEBUG LOGGER
